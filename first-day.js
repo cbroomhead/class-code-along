f0aa74e42ee2ca9200930f64c0b58a2efb92f197
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-04 21:15:20 +0000

    This is a comment for the sake of updating

---
+++ first-day.js
@@ -0,0 +1,35 @@
+
+//This is how you define variables
+var name = "Celine";
+var age ="28";
+
+
+//This fucntion will check whether or not the number is larger than zero
+var num = 10-;
+
+if (num > 0){
+    console.log('the number is greater than zero');
+    console.log('good job');
+}
+    
+    
+//functions
+
+/* these 3 different ways of writing functions are not 100% equivalent. 
+We will see the differences together later on. 
+Use the first one!!
+*/
+function doSomething(string){
+    //..code goes here
+}
+    
+var doSomething = finction(string){
+    //..code goes here
+}
+
+var doSomething = function doSomething(string){
+    //..code goes here
+}
+
+
+//This is a comment to write more stuff
